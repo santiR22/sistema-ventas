ec4fcbed4e9cf4d64408a1f062fa233c94cc9f2f
santiR22 <santiagorodas688@gmail.com>
2025-06-18 19:46:04 -0300

    aniadidas la tablas para imagenes, clientes, ventas y detalle de ventas

---
+++ database/migrations/2025_06_18_222609_create_detalle_venta_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detalle_venta', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('venta_id')->constrained('ventas')->cascadeOnDelete();
+            $table->foreignId('producto_id')->constrained('productos')->cascadeOnDelete();
+            $table->integer('cantidad');
+            $table->float('precio_unitario');
+            $table->float('subtotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detalle_venta');
+    }
+};
