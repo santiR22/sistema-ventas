4abee5f381425ccb2659fe448a4d9408d41b190b
santiR22 <santiagorodas688@gmail.com>
2025-06-19 20:07:11 -0300

    aniadidos controladores para auth y el dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Dashboard;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'index'])->name('login');
+Route::get('/', [Dashboard::class, 'index'])->name('home');
