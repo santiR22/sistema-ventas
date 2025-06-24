761619b38b23753bd4b0802c7beec56de2e15f36
santiR22 <santiagorodas688@gmail.com>
2025-06-24 07:59:58 -0300

    validaciones de rutas hechas

--- routes/web.php
+++ routes/web.php
@@ -11,42 +11,41 @@ use App\Http\Controllers\Ventas;
 use Illuminate\Support\Facades\Route;
 
 //crear un usuario admin, solo una vez
-Route::get('/crear-admin', [AuthController::class, 'createAdmin']);
+// Route::get('/crear-admin', [AuthController::class, 'createAdmin']);
 
-Route::middleware(['auth'])->group(function () {
-    // Rutas protegidas que requieren autenticación
-    Route::get('/dashboard', [Dashboard::class, 'index'])->name('dashboard');
-});
 
 Route::get('/', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('logear');
 
-//logout route
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Rutas protegidas que requieren autenticación
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [Dashboard::class, 'index'])->name('home');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
 
-Route::get('/home', [Dashboard::class, 'index'])->name('home');
 
 Route::prefix('ventas')->group(function () {
     Route::get('/nueva-venta', [Ventas::class, 'index'])->name('ventas-nueva');
 });
 
-Route::prefix('detalle')->group(function () {
+Route::prefix('detalle')->middleware('auth')->group(function () {
     Route::get('/detalle-venta', [DetalleVentas::class, 'index'])->name('detalle-venta');
 });
 
-Route::prefix('categorias')->group(function () {
+Route::prefix('categorias')->middleware('auth')->group(function () {
     Route::get('/', [Categorias::class, 'index'])->name('categorias');
 });
 
-Route::prefix('productos')->group(function () {
+Route::prefix('productos')->middleware('auth')->group(function () {
     Route::get('/', [Productos::class, 'index'])->name('productos');
 });
 
-Route::prefix('clientes')->group(function () {
+Route::prefix('clientes')->middleware('auth')->group(function () {
     Route::get('/', [Clientes::class, 'index'])->name('clientes');
 });
 
-Route::prefix('usuarios')->group(function () {
+Route::prefix('usuarios')->middleware('auth')->group(function () {
     Route::get('/', [Usuarios::class, 'index'])->name('usuarios');
 });
